7c0687a0c06c68767599651d7aed9eb882fa872b
bruno.oliveira <bruno.oliveira@sinax.com.br>
2016-11-08 11:24:41 -0200

    Criação de páginas e navegação

--- WebContent/view/js/controllers/vendasPlusController.js
+++ WebContent/view/js/controllers/vendasPlusController.js
@@ -1,3 +1,9 @@
 angular.module('vendasPlusApp', ['ngAnimate', 'ngAlertify', 'ui.bootstrap']).controller('mainCtrl', ['$scope', function($scope){
-  
+
+  $scope.menu = {};
+
+  $scope.menuSelected = function menuSelected(item){
+    $scope.menu.active = item
+    $scope.selectedTemplate = 'empresa/' + item + '.html';
+  };
 }]);
--- src/br/vp/rest/EmpresaRest.java
+++ src/br/vp/rest/EmpresaRest.java
@@ -5,19 +5,12 @@ import javax.ws.rs.Path;
 
 import br.vp.controller.EmpresaController;
 import br.vp.dto.EmpresaDTO;
- 
+
 @Path("/empresa")
 public class EmpresaRest {
- 
+
 	@GET
 	@Path("/getProdutos")
 	public String getProdutos() {
-		EmpresaDTO e = new EmpresaDTO(0, 9945, 1234, "JAX-RS", "CWB", "PR", "BRUNOLIN@EMAIL", "asdasd");
-		EmpresaController ec = new EmpresaController();		
-		if(ec.cadastroEmpresa(e)){
-			return "Deu boa";			
-		}
 		return "Deu ruim";
-	}
- 
-}
+  }
