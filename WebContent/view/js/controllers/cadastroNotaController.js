1a40dd82a16c578e74f3a4aef17dd768aa013760
bruno.oliveira <bruno.oliveira@sinax.com.br>
2017-10-27 11:40:56 -0200

    Ajuste upload foto Base64

--- WebContent/view/js/controllers/cadastroNotaController.js
+++ WebContent/view/js/controllers/cadastroNotaController.js
@@ -43,7 +43,7 @@ angular.module('vendasPlusApp').controller('cadastroNotaCtrl', ['$scope', '$http
   $scope.validForm = function() {
     return $scope.venda.idVenda && !!$scope.venda.data && ($scope.venda.idProduto || $scope.venda.idProduto == 0);
   }
-  
+
   $scope.toggleDropdown = function($event) {
     $event.preventDefault();
     $event.stopPropagation();
@@ -61,7 +61,7 @@ angular.module('vendasPlusApp').controller('cadastroNotaCtrl', ['$scope', '$http
       alertify.success('Nota adicionada!');
       $scope.venda = {};
 	  }, function(err) {
-        alertify.success('Nota não adicionada');        
+        alertify.success('Nota não adicionada');
     });
   }
 
